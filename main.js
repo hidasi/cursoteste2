775cfc6910a718bba8d7e912a926b1aef73ce322
José Hidasi-Neto <hidasineto@gmail.com>
2024-11-25 03:01:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    $('header button').click(function(){
+        $('form').slideDown();
+    });
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    });
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImg = $(`#link-nova-img`).val();
+        
+        const novoItem = $('<li style="display: none;"></li>');
+        $(`<img src="${enderecoNovaImg}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-link">
+                <a href="${enderecoNovaImg}" title='Ver a imagem em tamanho Real' target="_blank">Ver a imagem em tamanho Real</a>
+            </div>
+        `).appendTo(novoItem);
+        
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(800);
+        
+        // Limpa o campo de input após a adição
+        $('#link-nova-img').val('');
+    });
+});
